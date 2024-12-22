cde3eba48a1dbc843a906dd7226baf54c8c5cbf6
Crec0 <83436716+Crec0@users.noreply.github.com>
2024-12-21 22:07:23 -0800

    add icon and license

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
     import '../app.css';
+    import { Github } from 'lucide-svelte';
 
     let { children } = $props();
 </script>
 
 <div class="bg-background">
-    <div class="sticky z-50 flex w-full flex-col gap-1 border-b px-4 py-3 shadow-sm border-border/40">
-        <div class="text-2xl lg:text-4xl">Lottie to Webp</div>
-        <div class="text-xs lg:text-sm text-muted-foreground">Convert Lottie animation to Webp</div>
+    <div class="sticky z-50 flex w-full flex-row items-center gap-1 border-b px-4 py-3 shadow-sm border-border/40">
+        <div class="grow">
+            <div class="text-2xl lg:text-4xl">Lottie to Webp</div>
+            <div class="text-xs lg:text-sm text-muted-foreground">Convert Lottie animation to Webp</div>
+        </div>
+
+        <a href="https://github.com/Crec0/lottie-to-webp" target="_blank" class="w-8 md:w-12 h-8 md:h-12 fill-white">
+            <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+        </a>
     </div>
 
     <div class="p-4 flex flex-col items-center justify-center">
